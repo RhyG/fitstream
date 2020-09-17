4d4e1d5316f681b4e0a2d40646b0399408febd47
Rhys <rhysgeary@gmail.com>
2020-09-17 10:57:04 +1000

    Removed unused file and tidied comments

--- auth/auth.js
+++ auth/auth.js
@@ -34,20 +34,18 @@ passport.use(
     },
     async (login, password, done) => {
       try {
-        //Find the user associated with the login provided by the user
         const user = await User.findByLogin(login);
 
         if (!user) {
-          //If the user isn't found in the database, return a message
           return done(null, false, { message: "User not found" });
         }
-        //Validate password and make sure it matches with the corresponding hash stored in the database
-        //If the passwords match, it returns a value of true.
-        const validate = await user.validatePassword(password);
-        if (!validate) {
+
+        const isValid = await user.validatePassword(password);
+
+        if (!isValid) {
           return done(null, false, { message: "Wrong Password" });
         }
-        //Send the user information to the next middleware
+
         return done(null, user, { message: "Logged in Successfully" });
       } catch (error) {
         return done(error);
@@ -59,13 +57,11 @@ passport.use(
 passport.use(
   new JWTstrategy(
     {
-      // Secret used to sign JWT
       secretOrKey: process.env.JWT_SECRET,
       jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
     },
     async (token, done) => {
       try {
-        // Pass user details to next middleware
         return done(null, token.user);
       } catch (error) {
         done(error);
